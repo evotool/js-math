a6426707d764c864f433d94d2f1ceb7aa5157dd4
Shamil Sultanov <shamil.amirovich@gmail.com>
2022-01-20 23:23:13 +0300

    fix: update package to 0.3.0

--- src/index.ts
+++ src/index.ts
@@ -1,132 +1,152 @@
-const MathRandom = Math.random;
-const MathRound = Math.round;
-const MathCeil = Math.ceil;
-const MathFloor = Math.floor;
+
+const math_random = Math.random.bind(Math);
+const math_round = Math.round.bind(Math);
+const math_ceil = Math.ceil.bind(Math);
+const math_floor = Math.floor.bind(Math);
+const math_cos = Math.cos.bind(Math);
+const math_acos = Math.acos.bind(Math);
+const math_sin = Math.sin.bind(Math);
+const math_abs = Math.abs.bind(Math);
+const math_log = Math.log.bind(Math) as (x: number) => number;
 const $180pi = 180 / Math.PI;
 const $pi180 = Math.PI / 180;
 
-Object.defineProperties(Math, {
-	random: {
-		value(min?: number, max?: number): number {
-			if (min !== void 0) {
-				if (max === void 0) {
-					max = MathFloor.call(this, min);
-					min = 0;
-				} else {
-					max = MathFloor.call(this, max);
-					min = MathCeil.call(this, min);
-				}
+function random(): number;
+function random(max: number): number;
+function random(min: number, max: number): number;
+function random(min?: number, max?: number): number {
+  if (min !== void 0) {
+    if (max === void 0) {
+      max = math_floor(min);
+      min = 0;
+    } else {
+      max = math_floor(max);
+      min = math_ceil(min);
+    }
 
-				return MathFloor.call(this, (MathRandom.call(this) * (max - min)) + min);
-			}
+    return math_floor((math_random() * (max - min)) + min);
+  }
 
-			return MathRandom.call(this);
-		},
-		enumerable: false,
-		writable: false,
-	},
-	round: {
-		value(x: number, digits: number = 0): number {
-			if (digits > 0) {
-				digits = 10 ** MathRound.call(this, digits);
+  return math_random();
+}
 
-				return MathRound.call(this, x * digits) / digits;
-			}
+function round(x: number, digits: number = 0): number {
+  if (digits > 0) {
+    digits = 10 ** math_round(digits);
 
-			return MathRound.call(this, x);
-		},
-		enumerable: false,
-		writable: false,
-	},
-	ceil: {
-		value(x: number, digits: number = 0): number {
-			if (digits > 0) {
-				digits = 10 ** MathRound.call(this, digits);
+    return math_round(x * digits) / digits;
+  }
 
-				return MathCeil.call(this, x * digits) / digits;
-			}
+  return math_round(x);
+}
 
-			return MathCeil.call(this, x);
-		},
-		enumerable: false,
-		writable: false,
-	},
-	floor: {
-		value(x: number, digits: number = 0): number {
-			if (digits > 0) {
-				digits = 10 ** MathRound.call(this, digits);
+function ceil(x: number, digits: number = 0): number {
+  if (digits > 0) {
+    digits = 10 ** math_round(digits);
 
-				return MathFloor.call(this, x * digits) / digits;
-			}
+    return math_ceil(x * digits) / digits;
+  }
 
-			return MathFloor.call(this, x);
-		},
-		enumerable: false,
-		writable: false,
-	},
-	deg: {
-		value(radians: number): number {
-			return radians * $180pi;
-		},
-		enumerable: false,
-		writable: false,
-	},
-	rad: {
-		value(degrees: number): number {
-			return degrees * $pi180;
-		},
-		enumerable: false,
-		writable: false,
-	},
-	spha: {
-		value(locationA: Location, locationB: Location): number {
-			const rLatA = this.rad(locationA.latitude);
-			const rLatB = this.rad(locationB.latitude);
-			const rLonD = this.rad(this.abs(locationA.longitude - locationB.longitude));
-			const sin12 = this.sin(rLatA) * this.sin(rLatB);
-			const cos12 = this.cos(rLatA) * this.cos(rLatB);
-			const cosLamda = this.cos(rLonD);
+  return math_ceil(x);
+}
 
-			return this.deg(this.acos(sin12 + (cos12 * cosLamda))) as number;
-		},
-		enumerable: false,
-		writable: false,
-	},
-	sphd: {
-		value(radius: number, locationA: Location, locationB: Location): number {
-			return $pi180 * radius * this.spha(locationA, locationB);
-		},
-		enumerable: false,
-		writable: false,
-	},
-});
+function floor(x: number, digits: number = 0): number {
+  if (digits > 0) {
+    digits = 10 ** math_round(digits);
 
-export {};
+    return math_floor(x * digits) / digits;
+  }
+
+  return math_floor(x);
+}
+
+function sum(...xyz: number[]): number {
+  let sum = 0;
+
+  for (const v of xyz) {
+    sum += v;
+  }
+
+  return sum;
+}
+
+function mul(...xyz: number[]): number {
+  let mul = 1;
+
+  for (const v of xyz) {
+    mul *= v;
+  }
+
+  return mul;
+}
+
+function ln(x: number): number {
+  return math_log(x) / math_log(Math.E);
+}
+
+function log(x: number): number;
+function log(x: number, y: number): number;
+function log(x: number, y?: number): number {
+  return y === void 0 ? math_log(x) : math_log(y) / math_log(x);
+}
+
+function deg(radians: number): number {
+  return radians * $180pi;
+}
+
+function rad(degrees: number): number {
+  return degrees * $pi180;
+}
+
+/**
+ * Sphere angle
+ */
+function spha(locationA: Location, locationB: Location): number {
+  const rLatA = rad(locationA.latitude);
+  const rLatB = rad(locationB.latitude);
+  const rLonD = rad(math_abs(locationA.longitude - locationB.longitude));
+  const sin12 = math_sin(rLatA) * math_sin(rLatB);
+  const cos12 = math_cos(rLatA) * math_cos(rLatB);
+  const cosLamda = math_cos(rLonD);
+
+  return deg(math_acos(sin12 + (cos12 * cosLamda)))!;
+}
+
+/**
+ * Sphere distance
+ */
+function sphd(radius: number, locationA: Location, locationB: Location): number {
+  return $pi180 * radius * spha(locationA, locationB);
+}
+
+export {
+  random,
+  round,
+  ceil,
+  floor,
+  sum,
+  mul,
+  ln,
+  log,
+  deg,
+  rad,
+  spha,
+  sphd,
+};
 
 interface Location {
-	latitude: number;
-	longitude: number;
+  latitude: number;
+  longitude: number;
 }
 
-declare global {
-	interface Math {
-		random(min: number, max: number): number;
-		random(max: number): number;
-		random(): number;
-		round(x: number, digits?: number): number;
-		ceil(x: number, digits?: number): number;
-		floor(x: number, digits?: number): number;
-		deg(radians: number): number;
-		rad(degrees: number): number;
+/*
+fn: {
+	value<V extends { [name: string]: number }>(formula: string): (values: V) => number {
+		return new Function('return 0;') as (values: V) => number;
+	},
+	enumerable: false,
+	writable: false,
+},
 
-		/**
-		 * Sphere angle
-		 */
-		spha(locationA: Location, locationB: Location): number;
-
-		/**
-		 * Sphere distance
-		 */
-		sphd(radius: number, locationA: Location, locationB: Location): number;
-	}
-}
+fn<V extends { [name: string]: number }>(formula: string): (values: V) => number;
+ */
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,130 +1,131 @@
-import '../src';
+import { ceil, deg, floor, rad, random, round, spha, sphd } from '../src';
 
 describe('index', () => {
-	it('should run without errors', (done) => {
-		for (let i = 0; i < 1000; i++) {
-			const r = Math.random(100, 1000);
-			expect(Number.isInteger(r)).toBe(true);
-			expect(r).toBeLessThan(1000);
-			expect(r).toBeGreaterThanOrEqual(100);
-		}
+  it('should run without errors', (done) => {
+    // random
+    for (let i = 0; i < 1000; i++) {
+      const r = random(100, 1000);
+      expect(Number.isInteger(r)).toBe(true);
+      expect(r).toBeLessThan(1000);
+      expect(r).toBeGreaterThanOrEqual(100);
+    }
 
-		for (let i = 0; i < 100; i++) {
-			const r = Math.random(100);
-			expect(Number.isInteger(r)).toBe(true);
-			expect(r).toBeLessThan(100);
-			expect(r).toBeGreaterThanOrEqual(0);
-		}
+    for (let i = 0; i < 100; i++) {
+      const r = random(100);
+      expect(Number.isInteger(r)).toBe(true);
+      expect(r).toBeLessThan(100);
+      expect(r).toBeGreaterThanOrEqual(0);
+    }
 
-		for (let i = 0; i < 100; i++) {
-			const r = Math.random();
-			expect(r).toBeLessThan(1);
-			expect(r).toBeGreaterThanOrEqual(0);
-		}
+    for (let i = 0; i < 100; i++) {
+      const r = random();
+      expect(r).toBeLessThan(1);
+      expect(r).toBeGreaterThanOrEqual(0);
+    }
 
-		// Math.floor
+    // floor
 
-		let n = Math.floor(0.44444);
-		expect(n).toBe(0);
+    let n = floor(0.44444);
+    expect(n).toBe(0);
 
-		n = Math.floor(0.44444, 2);
-		expect(n).toBe(0.44);
+    n = floor(0.44444, 2);
+    expect(n).toBe(0.44);
 
-		n = Math.floor(0.55555, 2);
-		expect(n).toBe(0.55);
+    n = floor(0.55555, 2);
+    expect(n).toBe(0.55);
 
-		n = Math.floor(0.999, 2);
-		expect(n).toBe(0.99);
+    n = floor(0.999, 2);
+    expect(n).toBe(0.99);
 
-		// Math.round
+    // round
 
-		n = Math.round(0.44444);
-		expect(n).toBe(0);
+    n = round(0.44444);
+    expect(n).toBe(0);
 
-		n = Math.round(0.44444, 2);
-		expect(n).toBe(0.44);
+    n = round(0.44444, 2);
+    expect(n).toBe(0.44);
 
-		n = Math.round(0.55555, 2);
-		expect(n).toBe(0.56);
+    n = round(0.55555, 2);
+    expect(n).toBe(0.56);
 
-		// Math.ceil
+    // ceil
 
-		n = Math.ceil(0.44444);
-		expect(n).toBe(1);
+    n = ceil(0.44444);
+    expect(n).toBe(1);
 
-		n = Math.ceil(0.44444, 2);
-		expect(n).toBe(0.45);
+    n = ceil(0.44444, 2);
+    expect(n).toBe(0.45);
 
-		n = Math.ceil(0.55555, 2);
-		expect(n).toBe(0.56);
+    n = ceil(0.55555, 2);
+    expect(n).toBe(0.56);
 
-		n = Math.ceil(0.99000000, 2);
-		expect(n).toBe(0.99);
+    n = ceil(0.99000000, 2);
+    expect(n).toBe(0.99);
 
-		n = Math.ceil(0.99000001, 2);
-		expect(n).toBe(1);
+    n = ceil(0.99000001, 2);
+    expect(n).toBe(1);
 
-		n = Math.ceil(1.00000001, 2);
-		expect(n).toBe(1.01);
+    n = ceil(1.00000001, 2);
+    expect(n).toBe(1.01);
 
-		// Math.deg
+    // deg
 
-		n = Math.round(Math.deg(0), 14);
-		expect(n).toBe(0);
+    n = round(deg(0), 14);
+    expect(n).toBe(0);
 
-		n = Math.round(Math.deg(Math.PI), 14);
-		expect(n).toBe(180);
+    n = round(deg(Math.PI), 14);
+    expect(n).toBe(180);
 
-		n = Math.round(Math.deg(2 * Math.PI), 14);
-		expect(n).toBe(360);
+    n = round(deg(2 * Math.PI), 14);
+    expect(n).toBe(360);
 
-		n = Math.round(Math.deg(3 * Math.PI / 2), 14);
-		expect(n).toBe(270);
+    n = round(deg(3 * Math.PI / 2), 14);
+    expect(n).toBe(270);
 
-		n = Math.round(Math.deg(Math.PI / 2), 14);
-		expect(n).toBe(90);
+    n = round(deg(Math.PI / 2), 14);
+    expect(n).toBe(90);
 
-		n = Math.round(Math.deg(Math.PI / 4), 14);
-		expect(n).toBe(45);
+    n = round(deg(Math.PI / 4), 14);
+    expect(n).toBe(45);
 
-		n = Math.round(Math.deg(Math.PI / 6), 14);
-		expect(n).toBe(30);
+    n = round(deg(Math.PI / 6), 14);
+    expect(n).toBe(30);
 
-		n = Math.round(Math.deg(-Math.PI / 2), 14);
-		expect(n).toBe(-90);
+    n = round(deg(-Math.PI / 2), 14);
+    expect(n).toBe(-90);
 
-		// Math.rad
+    // rad
 
-		n = Math.rad(0);
-		expect(n).toBe(0);
+    n = rad(0);
+    expect(n).toBe(0);
 
-		n = Math.round(Math.rad(180), 14);
-		expect(n).toBe(Math.round(Math.PI, 14));
+    n = round(rad(180), 14);
+    expect(n).toBe(round(Math.PI, 14));
 
-		n = Math.round(Math.rad(360), 14);
-		expect(n).toBe(Math.round(2 * Math.PI, 14));
+    n = round(rad(360), 14);
+    expect(n).toBe(round(2 * Math.PI, 14));
 
-		n = Math.round(Math.rad(270), 14);
-		expect(n).toBe(Math.round(3 * Math.PI / 2, 14));
+    n = round(rad(270), 14);
+    expect(n).toBe(round(3 * Math.PI / 2, 14));
 
-		n = Math.round(Math.rad(90), 14);
-		expect(n).toBe(Math.round(Math.PI / 2, 14));
+    n = round(rad(90), 14);
+    expect(n).toBe(round(Math.PI / 2, 14));
 
-		n = Math.round(Math.rad(45), 14);
-		expect(n).toBe(Math.round(Math.PI / 4, 14));
+    n = round(rad(45), 14);
+    expect(n).toBe(round(Math.PI / 4, 14));
 
-		n = Math.round(Math.rad(30), 14);
-		expect(n).toBe(Math.round(Math.PI / 6, 14));
+    n = round(rad(30), 14);
+    expect(n).toBe(round(Math.PI / 6, 14));
 
-		n = Math.round(Math.rad(-90), 14);
-		expect(n).toBe(Math.round(-Math.PI / 2, 14));
+    n = round(rad(-90), 14);
+    expect(n).toBe(round(-Math.PI / 2, 14));
 
-		const a = Math.spha({ latitude: 0, longitude: 90 }, { latitude: 90, longitude: 0 });
-		expect(a).toBe(90);
+    const a = spha({ latitude: 0, longitude: 90 }, { latitude: 90, longitude: 0 });
+    expect(a).toBe(90);
 
-		const d = Math.sphd(6371, { latitude: 55.797416, longitude: 49.111374 }, { latitude: 55.795997, longitude: 49.127173 });
-		expect(Math.floor(d, 4)).toBe(1);
+    const d = sphd(6371, { latitude: 55.797416, longitude: 49.111374 }, { latitude: 55.795997, longitude: 49.127173 });
+    expect(floor(d, 4)).toBe(1);
 
-		done();
-	});
+    done();
+  });
 });
